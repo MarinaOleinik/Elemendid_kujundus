a5dad9dba1dcd1d3ba4e27894c33051b0400a966
marina.oleinik <marina.oleinik@HP-CZC6297BB4>
2022-02-04 10:32:47 +0200

    Add project files.

---
+++ Elemendid_kujundus/Editor_Page.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Elemendid_kujundus
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Editor_Page : ContentPage
+    {
+        public Editor_Page()
+        {
+            //InitializeComponent();
+            Editor editor = new Editor
+            {
+                Placeholder="Sisesta siia teksti",
+                PlaceholderColor=Color.Aqua,
+                BackgroundColor=Color.Beige,
+                TextColor=Color.Red
+                //FontSize=40
+            };
+            StackLayout st = new StackLayout();
+            st.BackgroundColor = Color.FromRgb(10, 50, 150);
+            st.Children.Add(editor);
+            Content = st;
+        }
+    }
+}
---
+++ Elemendid_kujundus/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Elemendid_kujundus
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Button editor_btn = new Button 
+            { 
+                Text="Editor leht",
+                TextColor=Color.Brown,
+                BackgroundColor=Color.Aqua   
+            };
+            editor_btn.Clicked += Editor_btn_Clicked;
+            StackLayout st = new StackLayout();
+            st.BackgroundColor = Color.FromRgb(50, 50, 50);
+            st.Children.Add(editor_btn);
+            Content = st;
+
+        }
+
+        private async void Editor_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Editor_Page());
+        }
+    }
+}
