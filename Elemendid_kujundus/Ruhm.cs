5dd86b1f9e7389a4fe2746985a64a5f10db78271
marina.oleinik <marina.oleinik@HP-CZC6297BB4>
2022-03-22 15:39:08 +0200

    Listid rühmades

---
+++ Elemendid_kujundus/Ruhm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace Elemendid_kujundus
+{
+    public class Ruhm<K,T> : ObservableCollection<T>
+    {
+        public K Nimetus { get; private set; }
+        public Ruhm(K nimetus, IEnumerable<T> items)
+        {
+            Nimetus = nimetus;
+            foreach (T item in items)
+                Items.Add(item);
+        }
+    }
+}
